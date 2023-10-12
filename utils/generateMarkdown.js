6241df6d14249a1bd4bac83fd1997e7009b50843
gfuselier <gisellefuselier@gmail.com>
2023-10-12 17:10:41 -0500

    finalized generate markdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -16,7 +16,11 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license) {
+    return `This project is ${license} licensed.`
+  } else { return ""}
+}
 
 // TODO: Create a function to generate markdown for README
 //use the github template and make some of the words template literals for what comes from user input
@@ -62,8 +66,8 @@ ${data.tests}
 
 ## Questions
 
-If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.`
-
-}
+If you have any questions about this repository, please contact me at <${data.email}>.
+To view my other projects, check out my [github profile](https://github.com/${data.username}).
+`}
 
 module.exports = generateMarkdown;
